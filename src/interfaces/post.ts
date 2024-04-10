3858fe87d37f7b12956e62ed620c2ca7e1baa347
Lagical <you@example.com>
2024-04-10 09:28:04 +0300

    changed news elements to fetch and display data from strapi

--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -2,10 +2,14 @@ export default interface Post {
   id: number;
   attributes: {
     title: string;
+    titleEn: string;
     description: string;
+    descriptionEn: string;
     createdAt: string;
     updatedAt: string;
     publishedAt: string;
+    postContent: string;
+    postContentEn: string;
     postImage: {
       data: {
         attributes: {
